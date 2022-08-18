f716a4b48ce7a0edfcaed00b41348e32217835f9
huypnd <105630508+huypnd@users.noreply.github.com>
2022-08-18 16:39:32 +0700

    update DB and some Querry

---
+++ AssignmentBl5/query.sql
@@ -0,0 +1,14 @@
+
+--1
+select ename,sum(DATEDIFF(HOUR,checkin,checkout))timework,(select sum(DATEDIFF(DAY,fromDate,toDate)) from request r where r.eid=e.eid group by r.eid) offtime from TimeSheet t join Employee e on t.eid=e.eid
+group by ename,e.eid
+;
+--2
+select eid ,sum(DATEDIFF(DAY,fromDate,toDate)) as DayOff from request r group by eid ;
+--3
+SELECT e.eid,e.ename,ISNULL(t.tid,-1) tid,
+t.checkin,t.checkout
+FROM Employee e LEFT JOIN 
+(SELECT * FROM TimeSheet WHERE checkin >= '2022-08-01'
+AND checkin <= '2022-08-20') t
+ON e.eid = t.eid
